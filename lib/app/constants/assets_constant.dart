bbe8feb6743756d6c2a416a2cf53584595f075bb
giadeveloper <giagamedeveloper@gmail.com>
2021-06-17 15:34:45 +0700

    add icon

--- lib/app/constants/assets_constant.dart
+++ lib/app/constants/assets_constant.dart
@@ -1,7 +1,27 @@
+abstract class FontConstant {
+  static const poppins = "Poppins";
+}
+
 abstract class ImageRasterConstant {
   //  static const _path = 'assets/images/raster';
 }
 
 abstract class ImageVectorConstant {
   // static const _path = 'assets/images/vector';
+}
+
+abstract class IconConstant {
+  static const _path = 'assets/icons';
+  static const album = _path + '/album.svg';
+  static const event = _path + '/event.svg';
+  static const hearth = _path + '/hearth.svg';
+  static const home = _path + '/home.svg';
+  static const menu = _path + '/menu.svg';
+  static const mic = _path + '/mic.svg';
+  static const music = _path + '/music.svg';
+  static const playlist = _path + '/playlist.svg';
+  static const radio = _path + '/radio.svg';
+  static const repeat = _path + '/repeat.svg';
+  static const search = _path + '/search.svg';
+  static const user = _path + '/user.svg';
 }
