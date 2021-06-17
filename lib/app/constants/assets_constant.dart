9e77bc5d65246971a175e1d83e6608f41cac91e1
giadeveloper <giagamedeveloper@gmail.com>
2021-06-17 11:26:10 +0700

    setup structure

---
+++ lib/app/constants/assets_constant.dart
@@ -0,0 +1,7 @@
+abstract class ImageRasterConstant {
+  //  static const _path = 'assets/images/raster';
+}
+
+abstract class ImageVectorConstant {
+  // static const _path = 'assets/images/vector';
+}
